2b3749a3af210b123a5841f99bac857417494e16
Japheth <jkarani171@gmail.com>
2024-03-24 11:38:29 -0600

    J

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    debugger;
+  }
+
+
+  return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+}
+
+wrapGifts(writeCards);
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+  return number;
+}
